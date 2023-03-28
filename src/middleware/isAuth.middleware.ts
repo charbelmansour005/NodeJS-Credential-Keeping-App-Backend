8d7b8b222f7dc4752fec7edb48382a054aeec40a
charbelmansour005 <charbelmansour005@gmail.com>
2023-03-29 01:54:48 +0300

    cleaned <> isAuth middleware

--- src/middleware/isAuth.middleware.ts
+++ src/middleware/isAuth.middleware.ts
@@ -2,18 +2,14 @@ import { RequestHandler } from "express"
 import { JwtPayload, verify } from "jsonwebtoken"
 import { ErrorResponse } from "../index.js"
 import { User } from "../models/user.model.js"
+import { createError } from "../utils/errorUtils.js"
 
 export const isAuth: RequestHandler = async (req, res, next) => {
   try {
     const authHeader = req.get("Authorization")
 
     if (!authHeader) {
-      const error: ErrorResponse = {
-        message: "Unauthenticated",
-        name: "Unauthenticated",
-        status: 401,
-      }
-      throw error
+      throw createError(401, "Unauthenticated", "Unauthenticated")
     }
 
     const access_token = authHeader.split(" ")[1]
@@ -22,33 +18,23 @@ export const isAuth: RequestHandler = async (req, res, next) => {
     try {
       decodedToken = verify(access_token, process.env.SECRET as string) as {
         userId: string
+        role: string
       }
       const user = await User.findOne({ _id: decodedToken.userId })
       if (user.logoutAll === true) {
-        const error: ErrorResponse = {
-          message: "Your password has been changed, please sign in again",
-          name: "Unauthenticated",
-          status: 401,
-        }
-        throw error
+        throw createError(
+          401,
+          "Unauthenticated",
+          "Your password has been changed, please sign in again"
+        )
       }
     } catch (error) {
       const { message, name } = error as Error
-      const thrownError: ErrorResponse = {
-        message: message,
-        name: name,
-        status: 409,
-      }
-      throw thrownError
+      throw createError(409, name, message)
     }
 
     if (!decodedToken) {
-      const error: ErrorResponse = {
-        message: "Unauthorized",
-        name: "Unauthorized",
-        status: 401,
-      }
-      throw error
+      throw createError(401, "Unauthorized", "Unauthorized")
     }
 
     const { userId } = decodedToken as JwtPayload
