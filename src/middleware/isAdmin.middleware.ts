c342d85978c51dc19b6e75c751fb5933c9b51bc5
charbelmansour005 <charbelmansour005@gmail.com>
2023-03-29 01:54:31 +0300

    added <> isAdmin middleware

---
+++ src/middleware/isAdmin.middleware.ts
@@ -0,0 +1,27 @@
+import { RequestHandler } from "express"
+import { JwtPayload, verify } from "jsonwebtoken"
+import { createError } from "../utils/errorUtils.js"
+
+export const isAdmin: RequestHandler = (req, res, next) => {
+  try {
+    const authHeader = req.get("Authorization")
+
+    if (!authHeader) {
+      throw createError(401, "Unauthenticated", "Unauthenticated")
+    }
+
+    const access_token = authHeader.split(" ")[1]
+    let decodedToken: string | JwtPayload
+    decodedToken = verify(access_token, process.env.SECRET as string) as {
+      userId: string
+      role: string
+    }
+
+    if (req.userId && decodedToken.role === "admin") {
+      return next()
+    }
+    throw createError(403, "Unauthorized", "Unauthorized")
+  } catch (error) {
+    next(error)
+  }
+}
